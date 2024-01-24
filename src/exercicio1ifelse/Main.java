7812173449fd6c31b51fbabd634694c64997a971
Lany Leite <Lanyleitee@gmail.com>
2024-01-23 23:07:37 -0300

    Exercício-04 - if/else

--- src/exercicio1ifelse/Main.java
+++ src/exercicio1ifelse/Main.java
@@ -8,22 +8,27 @@ public class Main {
 		
 		Scanner sc = new Scanner (System.in);
 		
-		System.out.println("Que horas são? ");
+		int codigo = sc.nextInt();
+		int quantidade = sc.nextInt();
 		
-		int hora = sc.nextInt();
-		
-		if (hora<12) {
-			System.out.println("Bom dia!");
+		double total;
+		if (codigo == 1) {
+			total = quantidade * 4.0;
 		}
-		else if (hora>=12 && hora<18) {
-			System.out.println("Boa Tarde! ");
-	
-			}
+		else if (codigo == 2) {
+			total = quantidade * 4.5;
+		}
+		else if (codigo == 3) {
+			total = quantidade * 5.0;
+		}
+		else if (codigo == 4) {
+			total = quantidade * 2.0;
+		}
 		else {
-			System.out.println("Boa Noite! ");
-		
-		
-		}		
+			total = quantidade * 1.5;
+		}
+
+		System.out.printf("Total: R$ %.2f%n", total);
 		
 		sc.close();
 	}
